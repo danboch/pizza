8b5416d38917917898cfdff39b7b01101efb7d84
Szymon Bubak <szymon.bubak@xtrf.eu>
2014-06-23 18:35:58 +0200

    order

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -1,7 +1,73 @@
+app.controller('OrderCtrl', function ($scope, order, $cookies) {
 
-app.controller('OrderCtrl', function ($scope, order) {
+  var username = $cookies.pizzauser;
 
   $scope.order = order;
 
+  function totalPieces(pieces) {
+
+    var total = 0;
+
+    angular.forEach(pieces, function (piece) {
+      total += piece;
+    });
+
+    return total;
+  }
+
+  $scope.addPiece = function (pizza) {
+
+    if (!pizza.hasOwnProperty('pieces')) {
+      pizza.pieces = {};
+    }
+
+    var pieces = pizza.pieces;
+
+    if (totalPieces(pieces) >= 8) return;
+
+    if (!angular.isUndefined(pieces[username])) {
+      pieces[username]++;
+    } else {
+      pieces[username] = 1;
+    }
+
+    order.$update(order);
+  }
+
+  $scope.removePiece = function (pizza) {
+
+    if (!pizza.hasOwnProperty('pieces')) {
+      pizza.pieces = {};
+    }
+
+    var pieces = pizza.pieces;
+
+    if (!angular.isUndefined(pieces[username]) && pieces[username] > 0) {
+      pieces[username]--;
+      order.$update(order);
+    }
+  }
+
+  $scope.addPizza = function (pizzaName) {
+
+    order.pizzas.push({name: pizzaName});
+
+    order.$update(order);
+
+  }
+
+  $scope.removePizza = function(pizza){
+
+    var index = order.pizzas.indexOf(pizza);
+
+    order.pizzas.splice(index,1);
+
+    order.$update(order);
+
+  }
+
+  $scope.updatePrice = function(){
+    order.$update(order);
+  }
 
 });
